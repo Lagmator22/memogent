d377afad21a88fb7d0be436cfbe3a55465f416a4
Lagmator22 <133108827+lagmator22@users.noreply.github.com>
2026-03-28 00:16:06 +0530

    Optimize memory usage

---
+++ bindings/android/settings.gradle.kts
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "memogent"
