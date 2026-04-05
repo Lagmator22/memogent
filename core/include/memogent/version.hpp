b918dd228fdf1e216da784b67248e9c246d7790f
Lagmator22 <133108827+lagmator22@users.noreply.github.com>
2026-04-06 04:26:01 +0530

    Refine API

---
+++ core/include/memogent/version.hpp
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+#pragma once
+
+namespace mem {
+inline constexpr int version_major = 0;
+inline constexpr int version_minor = 1;
+inline constexpr int version_patch = 0;
+inline constexpr const char* version_string = "0.1.0";
+}  // namespace mem
